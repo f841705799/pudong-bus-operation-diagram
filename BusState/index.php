2aa880915b1eb1b5e37a62e16975a89f38b4f121
Bob Liu <13636381320@163.com>
2020-02-13 06:08:43 +0800

    Add everything

--- BusState/index.php
+++ BusState/index.php
@@ -1,82 +1,75 @@
 <?php
-session_start();
-include('function.php');
-$zbh = $_GET['zbh'];
-if (!empty($zbh)){
-    $all = findbus($zbh);
-    $x = 0;
-    foreach ($all as $k1 => $v1)
-    {
 
-        $lineId[$x] = findbusline($v1);
-        $res[$x] = main($lineId[$x],$v1);
-        $x++;
-    }
-}
+/* The version is made by BobLiu0518 and I 've improved some UI issue for her. */
+
+	include('function.php'); //export function package
+	$zbh = $_GET['zbh'];
+	if(!empty($zbh))
+	{
+		$all = findbus($zbh);
+		$x = 0;
+		foreach ($all as $k1 => $v1)
+		{
+			$lineId[$x] = findbusline($v1);
+			$res[$x] = main($lineId[$x],$v1);
+			$x++;
+		}
+	}
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head id="Head1"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=0.5, maximum-scale=2.0, user-scalable=yes" /><title>
-	车辆状态查询
-</title><link rel="stylesheet" href="css/themes/default/jquery.mobile-1.1.1.css" /><link rel="stylesheet" href="css/formobile.css" />
-    <script type="text/javascript" src="Scripts/jquery-1.8.2.js"></script>
-
-    <script src="Scripts/jquery.mobile-1.2.0.min.js" type="text/javascript"></script>
-
-    <script type="text/javascript">
-        function SearchStation() {
-            if ($("#registrationmark").val().length > 3)
-            { $("#BtnSearch").click(); }
-            else
-            {
-                alert("输入过短！");
-            }
-
-
-        }
-    </script>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
+<title>车辆状态查询</title>
+<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+		body {
+			padding: 20px
+		}
+	</style>
 </head>
+
 <body>
-
-    <form name="form1" method="get" action="" id="form1">
-
-        <div class="ui-header ui-bar-d" role="banner" data-role="header" data-theme="d">
-            <h1 class="ui-title" role="heading" aria-level="1">车辆状态查询</h1>
-        </div>
-        <div class="ui-body">
-<form action="/bus/busstate/">
-<input name="zbh" id="zbh" type="text" placeholder="请输入自编" />
-<i class="search"></i>
-</form>
-
-        <p id="needShow">
-
-        
-
-<span id="lbCount" style="color:gray;">Max 10 results showed Only</span><br/><br/><br/><div id="result" style="min-width: 200px">
-    
-<?php 
-    $x = 0;
-    //print_r($all);
-    foreach ($res as $k1 => $v1)
-    {
-        echo '<div data-role="collapsible" data-theme="d" data-content-theme="d">'."\n";
-        echo  '<h3>'.$all[$x]."</h3>\n";
-        echo '<li class="ui-li ui-li-static ui-body-c" style="position: relative"><span>线路名</span><span style="left: 49%; position: absolute">'.get_line_name($lineId[$x]).'</span></li>'."\n";
-        echo '<li class="ui-li ui-li-static ui-body-c" style="position: relative"><span>车牌号</span><span style="left: 49%; position: absolute">'.find_plate($all[$x]).'</span></li>'."\n";
-        
-        foreach ($v1 as $k => $v)
-        {
-            echo '<li class="ui-li ui-li-static ui-body-c" style="position: relative"><span>'.$k.'</span><span style="left: 49%; position: absolute">'.$v.'</span></li>'."\n";
-        }
-    $x++;
-    echo "                            </ul>\n
-                        </p>\n
-                    </div>\n
-";
-    }
+<div class="container">
+<br />
+	<h2><b>车辆状态查询</b></h2>
+            <div class="search" class="row head">
+                <form action="index.php" method="get" class="form-inline">
+                    	<div class="col-lg-10">
+                        	<div class="input-group">
+                            		<input type="text" name="zbh" class="form-control" placeholder="在此自编号" value="<?= $zbh ?>" transparent autofocus x-webkit-speech>
+                            		<button type="submit" class="btn btn-primary" style="margin:2px;margin-top:0px">查询</button>
+                        	</div>
+                    	</div>
+                </form>
+            </div>
+</div>
+            
+<div class="container">
+<?php
+	$x = 0;
+	if(count($res) == 10)
+		echo '<div class="alert alert-info"><strong>注意：</strong>只显示前十条查询结果。</div>';
+	else if(!empty($zbh) && count($res) == 0)
+		echo '<div class="alert alert-danger"><strong>错误：</strong>没有找到结果，请检查输入是否正确。</div>';
+	foreach ($res as $k1 => $v1)
+	{
+		echo '<h3>'.$all[$x].'</h3>';
+		echo '<table class="table table-bordered"><tbody>';
+		echo '<tr><th colspan="2">车辆状态</th></tr>'."\n";
+		echo '<tr><td>线路名</td><td>'.get_line_name($lineId[$x]).'</td></tr>'."\n";
+		echo '<tr><td>车牌号</td><td>'.find_plate($all[$x]).'</td></tr>'."\n";
+		foreach ($v1 as $k => $v)
+		{
+			echo '<tr><td>'.$k.'</td><td>'.$v.'</td></tr>'."\n";
+		}
+		echo '</tbody></table>';
+	$x++;
+	}
 ?>
-
-    </form>
-</body>
+			</div>
+		</div>
+	</body>
 </html>
